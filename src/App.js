1b15ad116a421ff9b84e5cc5bdf8e7dabee71266
shayanfarzi <farzishayan@gmail.com>
2022-05-04 18:38:37 +0430

    check phantom wallet

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import './App.css';
-import twitterLogo from './assets/twitter-logo.svg';
+import React, { useEffect } from "react";
+import "./App.css";
+import twitterLogo from "./assets/twitter-logo.svg";
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = "_buildspace";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  const checkIfWalletConnected = async () => {
+    try {
+      const { solana } = window;
+      if (solana) {
+        if (solana.isPhantom) {
+          console.log("Phantom wallet found");
+        } else {
+          alert("Solana object not found! Get a phantom wallet");
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletConnected();
+    };
+    window.addEventListener("load", onLoad);
+    return () => window.removeEventListener("load", onLoad);
+  }, []);
   return (
-    <div className="App">
-      <div className="container">
-        <div className="header-container">
-          <p className="header">🍭 Candy Drop</p>
-          <p className="sub-text">NFT drop machine with fair mint</p>
+    <div className='App'>
+      <div className='container'>
+        <div className='header-container'>
+          <p className='header'>🍭 Candy Drop</p>
+          <p className='sub-text'>NFT drop machine with fair mint</p>
         </div>
-        <div className="footer-container">
-          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+        <div className='footer-container'>
+          <img alt='Twitter Logo' className='twitter-logo' src={twitterLogo} />
           <a
-            className="footer-text"
+            className='footer-text'
             href={TWITTER_LINK}
-            target="_blank"
-            rel="noreferrer"
+            target='_blank'
+            rel='noreferrer'
           >{`built on @${TWITTER_HANDLE}`}</a>
         </div>
       </div>
